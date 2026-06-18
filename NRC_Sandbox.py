ad04acd62b337488a80aba238cb1805681b47427
nicknockreynaldo <nicknock.reynaldo94@gmail.com>
2026-06-18 08:33:03 +0700

    Update NRC_Sandbox.py

--- NRC_Sandbox.py
+++ NRC_Sandbox.py
@@ -47,7 +47,7 @@ if 'memori_saham' not in st.session_state:
 
 # --- KONTROL MENU UTAMA (TABS) ---
 # DITAMBAHKAN tab_calc
-tab_screener, tab_watchlist, tab_calc, tab_journal = st.tabs(["🚀 Screener", "📋 Watchlist", "🧮 Calculator", "📊 Journal"])
+tab_screener, tab_watchlist, tab_calc, tab_active_trade = st.tabs(["🚀 Screener", "📋 Watchlist", "🧮 Calculator", "📊 Portfolio"])
 
 # ==============================================================================
 # TAB 1: CODE ASLI SANDBOX (DIPERTAHANKAN SEPENUHNYA)
